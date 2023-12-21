e16c4e80b74e9ee45885979c2764d2a3c9bdb7e3
Reed Benjamin <reed@Reeds-MacBook-Pro.local>
2023-12-21 17:04:49 -0500

    Started for loop to compare current hour with each time block - needs fixing

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,15 @@ console.log(div)
 const today = dayjs().format('dddd, MMMM D, YYYY')
 console.log(today)
 
+// use dayJS to get current hour and store that in a variable 
+
+const hour = dayjs().format('H:mm')
+console.log(hour)
+
+const divs = Array.from(document.querySelectorAll('.time-block')) 
+console.log(divs)
+// this is an array
+
 // append to page
     // create an element
     // set the inner text to that variable I just made
@@ -23,13 +32,29 @@ div.appendChild(h3)
   
 displayDate()
 
-// use dayJS to get current hour and store that in a variable 
-
-// const divs = document.querySelectorAll('.py-3') 
-// this is an array
-
 // for loop
     // get inner text from each div
+for (let i = 0; i < divs.length; i++) {
+    const innerText = divs[i].innerText
+    console.log(innerText)
+
+    if (innerText < hour) {
+        divs.forEach(div => {
+            div.setAttribute('class', 'row time-block past')
+        })
+    }
+    else if (innerText === hour) {
+        divs.forEach(div => {
+            div.setAttribute('class', 'row time-block present')
+        })
+    }
+    else {
+        divs.forEach(div => {
+            div.setAttribute('class', 'row time-block future')
+        })
+    }
+}
+
     // compare that to the current hour (if statement)
         // .setAttribute('class', 'row time-block past')
     // if inner text is equal to the current hour
