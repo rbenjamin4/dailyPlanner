cfeda09b2fd5dbbfe44ff4b58fd61cf35d6aeadd
Reed Benjamin <reed@Reeds-MacBook-Pro.local>
2023-12-31 14:50:04 -0500

    Cleaned up code and pseudocode and updated comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,16 @@
-// show today's date
-// format dayJS
-// get today's date in the right format and save it to variable
-// console.log variable
 
 const div = document.querySelector('#currentDay')
-console.log(div)
+
+// obtain today's date and format dayJS to save in a variable
 const today = dayjs().format('dddd, MMMM D, YYYY')
-console.log(today)
 
 // use dayJS to get current hour and store that in a variable 
-
 const hour = dayjs().format('H')
-console.log(hour)
 
 // obtain all divs that have time-block class
 const timeBlocks = document.querySelectorAll('.time-block')
 
-// append to page
-    // create an element
-    // set the inner text to today variable
-    // append that to the currentDay div
-
+// create element and display today's date by appending to the currentDay div
 const displayDate = () => {
     
 const h3 = document.createElement('h3')
@@ -32,7 +22,6 @@ div.appendChild(h3)
 displayDate()
 
 // get id for each hour block and compare that to current hour to color code each hour block
-
 const setBlockColor = () => {
 
     timeBlocks.forEach(function(block) {
@@ -56,17 +45,17 @@ setBlockColor()
 // save text entries in local storage
 const textEntry = () => {
     $('.saveBtn').on('click', function() {
-      const key = $(this).parent().attr('id');
-      const value = $(this).siblings('.description').val();
-      localStorage.setItem(key, value);
-    });
+      const key = $(this).parent().attr('id')
+      const value = $(this).siblings('.description').val()
+      localStorage.setItem(key, value)
+    })
   }
 
 // retrieve text entries from local storage when page is refreshed
 $('.time-block').each(function() {
-    const key = $(this).attr('id');
-    const value = localStorage.getItem(key);
-    $(this).children('.description').val(value);
-  });
+    const key = $(this).attr('id')
+    const value = localStorage.getItem(key)
+    $(this).children('.description').val(value)
+  })
 
   textEntry()
